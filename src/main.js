f0838941b98f672f442e894360a1a6cadff2547e
Thuong Ho <thuong.t.ho@gmail.com>
2016-06-04 11:23:58 -0700

    grab full screen template from phaser-master and prepopulate Boot.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+    var GAME_WIDTH = 640;
+    var GAME_HEIGHT = 360;
+
+    var phaserGame = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'gameContainer');
+})();
