b481f98eadbf8d6a59be4d75005f7f3d8a23f1f6
Thuong Ho <thuong.t.ho@gmail.com>
2016-06-18 13:37:59 -0700

    add character to game

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,12 +1,22 @@
 'use strict';
 
-Hunted.Game = function(game) {
-
-}
+Hunted.Game = function (game) {
+    this.character;
+};
 
 Hunted.Game.prototype = {
 
-    create: function () {},
+    create: function () {
+
+        this.buildWorld();
+
+    },
+
+    buildWorld: function () {
+        this.character = this.add.image(this.world.centerX, this.world.centerY, 'male_player');
+        this.character.anchor.setTo(0.5);
+        this.character.scale.setTo(0.2);
+    },
 
     update: function () {}
-}
+};
